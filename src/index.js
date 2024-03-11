0a1201f6ff499c5ed95e3c942f5d1655a42a1ac3
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-03-11 01:05:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './App'; // Asegúrate de importar el componente App
 
 const root = createRoot(document.getElementById('root'));
 
