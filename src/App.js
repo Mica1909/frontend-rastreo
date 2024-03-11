394ad0f165f68a655c95854e8d9c6c6247f15398
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-03-11 00:41:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
 function App() {
+  return (
     <Router>
       <div className="App">
         <Header />
@@ -17,4 +21,8 @@ function App() {
         <Footer />
       </div>
     </Router>
+  );
+}
+
+export default App;
 
